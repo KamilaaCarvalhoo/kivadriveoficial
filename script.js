ae2913a0698c1d915f4ffa09f2eae9b04946fcb3
kamilaacarvalhoo <kamilacarvalho9929@gmail.com>
2026-03-14 10:02:16 -0300

    feitoo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /* BOTÕES DE DOWNLOAD */
 
-window.baixarMotorista = function () {
+window.baixarPassageira = function () {
     window.location.href = "https://www.dropbox.com/scl/fi/75ona2vur50naamhwzf9q/Kiva-Drive.apk?rlkey=tz4k421np4172fjhzv0uidy9q&st=sknbkb8m&dl=1";
 };
 

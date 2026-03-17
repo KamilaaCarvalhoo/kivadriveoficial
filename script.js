450e8ce07183198a3ad6ebfc8db1ebf2023f1a02
kamilaacarvalhoo <kamilacarvalho9929@gmail.com>
2026-03-17 08:35:48 -0300

    backupp

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.baixarPassageira = function () {
 };
 
 window.baixarMotorista = function () {
-    window.location.href = "https://www.dropbox.com/scl/fi/epieqbek1vhpvos5xr8ci/Kiva-Motoristas.apk?rlkey=wki4pa91rdlkegalkdw6fiude&st=lsxf3ico&dl=1";
+    window.location.href = "https://dl.dropboxusercontent.com/scl/fi/epieqbek1vhpvos5xr8ci/Kiva-Motoristas.apk?rlkey=wki4pa91rdlkegalkdw6fiude&st=lsxf3ico&dl=1";
 };
 
 

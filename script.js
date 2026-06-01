bad8cf175b7831723e6b59a0cf9546deb3d4a521
kamilaacarvalhoo <kamilacarvalho9929@gmail.com>
2026-06-01 16:53:07 -0300

    adicionando

--- script.js
+++ script.js
@@ -1,113 +1,81 @@
 document.addEventListener("DOMContentLoaded", function () {
+  /* BOTÕES DE DOWNLOAD */
 
-/* BOTÕES DE DOWNLOAD */
+  window.baixarPassageira = function () {
+    window.location.href =
+      "https://play.google.com/store/apps/details?id=com.kivadrive.passageira";
+  };
 
-window.baixarPassageira = function () {
-    window.location.href = //"https://dl.dropboxusercontent.com/scl/fi/jmn7hi8md9r5ge0aj1whm/Kiva-Drive.apk?rlkey=sczaee06m93le3ca8gl1lm0i3"
-};
+  window.baixarMotorista = function () {
+    window.location.href =
+      "https://play.google.com/store/apps/details?id=com.kivamotorista.kivaapp";
+  };
 
-window.baixarMotorista = function () {
-    window.location.href = //"https://dl.dropboxusercontent.com/scl/fi/oxv9jyusnefoj3acwuwmk/Kiva-Motorista.apk?rlkey=p485tef0ku8p5voyl51s8331p"
-};
+  /* ANIMAÇÃO AO ROLAR */
 
-
-
-/* ANIMAÇÃO AO ROLAR */
-
-function reveal(){
-
+  function reveal() {
     const reveals = document.querySelectorAll(".reveal");
 
-    reveals.forEach((element)=>{
+    reveals.forEach((element) => {
+      const windowHeight = window.innerHeight;
+      const elementTop = element.getBoundingClientRect().top;
+      const revealPoint = 100;
 
-        const windowHeight = window.innerHeight;
-        const elementTop = element.getBoundingClientRect().top;
-        const revealPoint = 100;
+      if (elementTop < windowHeight - revealPoint) {
+        element.classList.add("active");
+      }
+    });
+  }
 
-        if(elementTop < windowHeight - revealPoint){
-            element.classList.add("active");
-        }
+  window.addEventListener("scroll", reveal);
 
+  const mapElement = document.getElementById("map");
+
+  if (mapElement) {
+    var map = L.map("map").setView([-16.6864, -49.2643], 12);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap",
+    }).addTo(map);
+
+    setTimeout(function () {
+      map.invalidateSize();
+    }, 200);
+
+    var carIcon = L.icon({
+      iconUrl: "https://cdn-icons-png.flaticon.com/512/744/744465.png",
+      iconSize: [40, 40],
+      iconAnchor: [20, 20],
     });
 
-}
+    var car = L.marker([-16.6864, -49.2643], { icon: carIcon }).addTo(map);
 
-window.addEventListener("scroll", reveal);
+    var route = [
+      [-16.6864, -49.2643],
+      [-16.69, -49.27],
+      [-16.695, -49.26],
+      [-16.7, -49.255],
+      [-16.705, -49.25],
+    ];
 
+    var i = 0;
 
+    function moveCar() {
+      car.setLatLng(route[i]);
 
+      i++;
 
-const mapElement = document.getElementById("map");
+      if (i >= route.length) {
+        i = 0;
+      }
+    }
 
-if(mapElement){
+    setInterval(moveCar, 2000);
 
-var map = L.map("map").setView([-16.6864,-49.2643],12);
+    L.marker([-16.69, -49.27]).addTo(map).bindPopup("🚗 Motorista disponível");
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-attribution:"© OpenStreetMap"
-}).addTo(map);
+    L.marker([-16.695, -49.26]).addTo(map).bindPopup("📍 Corrida iniciada");
 
-
-
-setTimeout(function(){
-map.invalidateSize();
-},200);
-
-
-
-
-var carIcon = L.icon({
-iconUrl:"https://cdn-icons-png.flaticon.com/512/744/744465.png",
-iconSize:[40,40],
-iconAnchor:[20,20]
-});
-
-
-
-var car = L.marker([-16.6864,-49.2643],{icon:carIcon}).addTo(map);
-
-
-
-
-var route=[
-[-16.6864,-49.2643],
-[-16.6900,-49.2700],
-[-16.6950,-49.2600],
-[-16.7000,-49.2550],
-[-16.7050,-49.2500]
-];
-
-var i=0;
-
-
-
-
-function moveCar(){
-
-car.setLatLng(route[i]);
-
-i++;
-
-if(i>=route.length){
-i=0;
-}
-
-}
-
-setInterval(moveCar,2000);
-
-
-
-
-L.marker([-16.6900,-49.2700]).addTo(map)
-.bindPopup("🚗 Motorista disponível");
-
-L.marker([-16.6950,-49.2600]).addTo(map)
-.bindPopup("📍 Corrida iniciada");
-
-L.marker([-16.7000,-49.2550]).addTo(map)
-.bindPopup("🧭 Corrida em andamento");
-
-}
-
+    L.marker([-16.7, -49.255]).addTo(map).bindPopup("🧭 Corrida em andamento");
+  }
 });
